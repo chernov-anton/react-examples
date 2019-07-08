f2f01b9c376f5524f1f37cc12bbcd019330879e1
Anton Chernov <chernov.anton.dev@gmail.com>
2019-07-08 15:59:26 +0300

    feat: add bootstrap

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+STATIC VERSION
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
 class ProductCategoryRow extends React.Component {
     render() {
